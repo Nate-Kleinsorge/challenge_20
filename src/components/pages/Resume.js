8cffa530b40462af37a59e698ee9cbc3f741640e
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-03-31 10:22:00 -0400

    added screenshot of resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,11 @@
 import { Helmet } from "react-helmet";
+import resume from '../../assets/resume-screenshot.png';
+
+const styles = {
+    image: {
+        margin: '25px'
+    }
+}
 
 export default function Resume() {
     return (
@@ -7,7 +14,12 @@ export default function Resume() {
             <title>Nate Kleinsorge | Resume</title>
         </Helmet>
         <div>
-            <h1 className="text-center text-white header">Resume</h1>
+            <h1 className="text-center text-white header border-bottom">Resume</h1>
+            <div className="d-flex flex-column text-center">
+                <div className="p-2">
+                    <img style={styles.image} src={resume} alt="screenshot of nate kleinsorges resume"></img>
+                </div>
+            </div>
         </div>
         </>
     );
